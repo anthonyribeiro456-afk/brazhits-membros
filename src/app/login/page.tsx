abb264e21aad6f19883576a8ecd32f4b0b6b9642
anthony. <anthony.ribeiro456@gmail.com>
2025-10-13 19:40:13 +0000

    Reduza o espaço superior entre a logo e o topo da tela, mantendo a logo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -60,7 +60,7 @@ export default function LoginPage() {
   }
 
   return (
-          <div className="flex flex-col items-center justify-center min-h-screen bg-background">
+          <div className="flex flex-col items-center justify-center min-h-screen bg-background -translate-y-6">
 
           <img
             src="/imgs/logo_braz.png"
