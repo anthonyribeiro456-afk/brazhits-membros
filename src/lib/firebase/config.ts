35fb6f53744892a1d34252eb50a096e3c35b6f78
anthony. <anthony.ribeiro456@gmail.com>
2025-10-14 13:20:07 +0000

    File changes

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,12 +3,11 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "YOUR_API_KEY",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "YOUR_PROJECT_ID",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "YOUR_STORAGE_BUCKET",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "YOUR_MESSAGING_SENDER_ID",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "YOUR_APP_ID",
+  projectId: "studio-1962441652-b79bb",
+  appId: "1:295694764655:web:7273987eb6c1a754eedb9a",
+  apiKey: "AIzaSyA4qtPj8KUxw6vn2EqQijxo8Bk8BsvC_ck",
+  authDomain: "studio-1962441652-b79bb.firebaseapp.com",
+  messagingSenderId: "295694764655",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
